d4944435b0708a12e4c9269a4a054291ba4a0bd0
Dean Gilewicz <gilewiczd@gmail.com>
2018-02-26 23:04:18 +0000

    basic header constructed

---
+++ wp-content/themes/retrospec/front-page.php
@@ -0,0 +1,259 @@
+<?php 
+	// THIS IS THE TEMPLATE FOR FRONT PAGE
+	
+	// begin set up hero img slider feature
+	$argsSlider = array(
+		'posts_per_page' => '-1',
+		'tag' => 'featured'
+	);
+	$the_query_slider = new WP_Query( $argsSlider );
+	// end set up
+	
+	// begin all latest posts section (not in featured tag)
+	$argsAllPosts = array(
+		'post_type' => array('post'),
+		'category__not_in' => '344',
+		'tag__not_in' => '10',
+		'posts_per_page' => '3',
+		'orderby' => 'date'
+	);
+	$the_query_all_posts = new WP_Query( $argsAllPosts );
+	// end all latest posts section
+?>
+
+<?php get_header(); ?>
+
+<!-- begin slider -->
+
+<?php if ( $the_query_slider->have_posts() ) : ?>
+
+	<?php $postNum = 0; ?>
+
+	<div class="slider">
+
+		<div class="slides">
+		
+			<?php while ( $the_query_slider->have_posts() ) : $the_query_slider->the_post(); ?>
+				
+				<?php 
+					$postNum++;
+					$thumb = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'large');
+					$url = $thumb['0'];
+				?>
+
+				<div class="slide-<?= $postNum; ?> <?= ($postNum == 1) ? 'active' : ''; ?>" style="background-image: url('<?= $url; ?>');">
+					<div class="mask"></div>
+					<div class="row">
+						<div class="small-12 columns">
+							<div class="content-of-slide">
+
+								<h2><?php the_title(); ?></h2>
+								<?php the_excerpt(); ?>
+								<a class="button mobile-slider" href="<?php the_permalink(); ?>">go</a>
+
+							</div>
+						</div>
+					</div>
+				</div>
+
+			<?php endwhile; ?>
+
+		</div>
+
+		<div class="progress-of-slides">
+			<div class="row">
+				<div class="small-12 columns">
+
+				<?php $postNum = 0; ?>
+
+				<?php while ( $the_query_slider->have_posts() ) : $the_query_slider->the_post(); ?>
+
+					<?php $postNum++; ?>
+
+					<div class="progress-btn <?php echo ($postNum == 1) ? 'active' : ''; ?>"><?php the_title(); ?></div>
+
+				<?php endwhile; ?>
+
+				</div>
+			</div>
+		</div>
+
+	</div>
+
+	<?php wp_reset_postdata(); ?>
+
+<?php else : ?>
+
+	<div class="slider">
+
+		<div class="slides">
+
+			<div class="slide- active" style="background-color: rgba(0,0,0,0.7);">
+				<div class="mask"></div>
+				<div class="row">
+					<div class="small-12 columns">
+						<div class="content-of-slide">
+
+							<h2>no posts to display</h2>
+							<p>this is the excerpt text</p>
+							<a class="button mobile-slider" href="/">go</a>
+
+						</div>
+					</div>
+				</div>
+			</div>
+
+		</div>
+
+	</div>
+
+<? endif; ?>
+
+<!-- end slider -->
+
+<!-- begin all latest posts (except chosen as home post) -->
+
+<section class="block-1">
+
+	<?php if ( $the_query_all_posts->have_posts() ) : ?>
+
+		<?php $counter = 0; ?>
+
+		<?php while ( $the_query_all_posts->have_posts() ) : $the_query_all_posts->the_post(); ?>
+			
+			<?php 
+				$postType = get_post_type_object(get_post_type());
+			?>
+
+			<?php if ($counter % 2 === 0) : ?>
+
+				<article class="row home-post ltr">
+					<div class="medium-7 medium-push-5 columns">
+						<div class="post-meta">
+							<span class="post-meta-category">
+								<a href="/<?= strtolower(str_replace(" ", "-", $postType->labels->name)); ?>"><?= $postType->labels->name; ?></a>
+							</span>
+							<span class="post-meta-date"><?php echo get_the_date('M j, Y'); ?></span>
+							<!-- <span class="post-meta-comments"><?php comments_number( '0', '1', '%' ); ?></span> -->
+						</div>
+						<a href="<?php the_permalink(); ?>" class="post-title"><h2><?php the_title(); ?></h2></a>
+						<div class="post-excerpt">
+							<?php the_excerpt(); ?>
+						</div>
+						<a href="<?php the_permalink(); ?>" class="read-more">Read More</a>
+					</div>
+					<div class="medium-5 medium-pull-7 columns">
+						<a href="<?php the_permalink(); ?>">
+							<?php the_post_thumbnail('large'); ?>
+						</a>
+					</div>
+				</article>
+
+			<?php else: ?>
+
+				<article class="row home-post rtl">
+					<div class="medium-7 columns">
+						<div class="post-meta">
+							<!-- <span class="post-meta-comments"><?php comments_number( '0', '1', '%' ); ?></span> -->
+							<span class="post-meta-date"><?php echo get_the_date('M j, Y'); ?></span>
+							<span class="post-meta-category">
+								<a href="/<?= strtolower(str_replace(" ", "-", $postType->labels->name)); ?>"><?= $postType->labels->name; ?></a>
+							</span>
+						</div>
+						<a href="<?php the_permalink(); ?>" class="post-title"><h2><?php the_title(); ?></h2></a>
+						<div class="post-excerpt">
+							<?php the_excerpt(); ?>
+						</div>
+						<a href="<?php the_permalink(); ?>" class="read-more">Read More</a>
+					</div>
+					<div class="medium-5 columns">
+						<a href="<?php the_permalink(); ?>">
+							<?php the_post_thumbnail('large'); ?>
+						</a>
+					</div>
+				</article>
+
+			<?php endif; ?>
+
+			<?php $counter++; ?>
+
+		<?php endwhile; ?>
+		
+	<?php else: ?>
+
+		<!-- <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p> -->
+
+		<article class="row home-post ltr">
+			<div class="medium-7 medium-push-5 columns">
+				<div class="post-meta">
+					<span class="post-meta-category">
+						<a href="/">category name</a>
+					</span>
+					<span class="post-meta-date"><?= date('M j, Y'); ?></span>
+				</div>
+				<a href="/" class="post-title"><h2>post title</h2></a>
+				<div class="post-excerpt">
+					<p>The post excerpt goes here</p>
+				</div>
+				<a href="/" class="read-more">Read More</a>
+			</div>
+			<div class="medium-5 medium-pull-7 columns">
+				<a href="/">
+					<img src="<?= get_stylesheet_directory_uri(); ?>/dist/icons/logo/DOD-icon.png" alt="destinations on a dash logo" />
+				</a>
+			</div>
+		</article>
+
+		<article class="row home-post rtl">
+			<div class="medium-7 columns">
+				<div class="post-meta">
+					<span class="post-meta-date"><?= date('M j, Y'); ?></span>
+					<span class="post-meta-category">
+						<a href="/">category name</a>
+					</span>
+				</div>
+				<a href="/" class="post-title"><h2>post title</h2></a>
+				<div class="post-excerpt">
+					<p>The post excerpt goes here</p>
+				</div>
+				<a href="/" class="read-more">Read More</a>
+			</div>
+			<div class="medium-5 columns">
+				<a href="/">
+					<img src="<?= get_stylesheet_directory_uri(); ?>/dist/icons/logo/DOD-icon.png" alt="destinations on a dash logo" />
+				</a>
+			</div>
+		</article>
+
+		<article class="row home-post ltr">
+			<div class="medium-7 medium-push-5 columns">
+				<div class="post-meta">
+					<span class="post-meta-category">
+						<a href="/">category name</a>
+					</span>
+					<span class="post-meta-date"><?= date('M j, Y'); ?></span>
+				</div>
+				<a href="/" class="post-title"><h2>post title</h2></a>
+				<div class="post-excerpt">
+					<p>The post excerpt goes here</p>
+				</div>
+				<a href="/" class="read-more">Read More</a>
+			</div>
+			<div class="medium-5 medium-pull-7 columns">
+				<a href="/">
+					<img src="<?= get_stylesheet_directory_uri(); ?>/dist/icons/logo/DOD-icon.png" alt="destinations on a dash logo" />
+				</a>
+			</div>
+		</article>
+
+	<?php endif; ?>
+
+	<?php wp_reset_postdata(); ?>
+
+</section>
+
+<!-- end all latest posts -->
+
+<?php // get_sidebar(); ?>
+
+<?php get_footer(); ?>
--- wp-content/themes/retrospec/header.php
+++ wp-content/themes/retrospec/header.php
@@ -10,102 +10,78 @@
  */
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?> class="no-js">
-<head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>" />
-	<meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=1">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-	<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto+Mono|Rubik:900" />
-	<link rel="stylesheet" type="text/css" href="<?= get_stylesheet_directory_uri(); ?>/dist/css/main.min.css" />
-	<!-- <link rel="profile" href="http://gmpg.org/xfn/11"> -->
-	<!-- <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>"> -->
-	<!--[if lt IE 9]>
-	<script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/html5.js"></script>
-	<![endif]-->
-	<?php wp_head(); ?>
 
-	<script>
-		// google analytics here
-	</script>
+	<head>
+		<meta charset="<?php bloginfo( 'charset' ); ?>" />
+		<meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=1">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto+Mono|Rubik:900" />
+		<link rel="stylesheet" type="text/css" href="<?= get_stylesheet_directory_uri(); ?>/dist/css/main.min.css" />
+		<!-- <link rel="profile" href="http://gmpg.org/xfn/11"> -->
+		<!-- <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>"> -->
+		<!--[if lt IE 9]>
+		<script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/html5.js"></script>
+		<![endif]-->
+		<?php wp_head(); ?>
 
-</head>
+		<script>
+			// google analytics here
+		</script>
+	</head>
 
-<body <?php body_class(); ?>>
+	<body <?php body_class(); ?>>
 
-	<div class="site_wrapper">
+		<div class="site__wrapper"><!-- .site__wrapper -->
 
-		<div id="page" class="hfeed site">
-	
-		<!-- <a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'retrospec' ); ?></a> -->
+			<div id="page" class="hfeed site"><!-- page wrapper -->
 		
-		<div class="container_global_menu">
+				<!-- <a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'retrospec' ); ?></a> -->
+				
+				<div class="global__menu">
 
-			<nav>
+					<nav>
 
-				<form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-					<!-- <label> -->
-						<!-- <span class="screen-reader-text"><?php echo _x( 'Search for:', 'label', 'retrospec' ); ?></span> -->
-					<div class="container_search_input">
-						<input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', 'retrospec' ); ?>" value="<?php echo get_search_query(); ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label', 'retrospec' ); ?>" />
-						<!-- </label> -->
-						<button type="submit" class="search-submit button">
-							<!-- <span class="screen-reader-text"><?php echo _x( 'Search', 'submit button', 'retrospec' ); ?></span> -->
-							<i class="genericon genericon-search"></i>
-						</button>
-					</div>
-				</form>
+						<!-- <form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+							<div class="container_search_input">
+								<input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', 'retrospec' ); ?>" value="<?php echo get_search_query(); ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label', 'retrospec' ); ?>" />
+								<button type="submit" class="search-submit button">
+									Go
+								</button>
+							</div>
+						</form> -->
 
-				<ul>
-					<li class="<?php if( is_home() ) { echo 'current-cat'; } ?>">
-		                <a href="/">home</a>
-		            </li>
-					<li class="<?php if( is_page('about-me') ) { echo 'current-cat'; } ?>">
-		                <a href="/about-me" class="">about me</a>
-		            </li>
-				<?php 
-					wp_list_categories( array(
-						'orderby' => 'name',
-						'title_li' => '',
-						'hide_title_if_empty' => true
-					) );
-				?>
-				</ul>
-		    </nav>
+						<ul>
+							<li class="<?php if( is_home() ) { echo 'current-cat'; } ?>">
+				                <a href="/">home</a>
+				            </li>
+							<li class="<?php if( is_page('about-me') ) { echo 'current-cat'; } ?>">
+				                <a href="/about-me" class="">about me</a>
+				            </li>
+				            <li class="<?php if( is_page('featured-fridays') ) { echo 'current-cat'; } ?>">
+				                <a href="/featured-fridays" class="">featured fridays</a>
+				            </li>
+				            <li class="<?php if( is_page('events') ) { echo 'current-cat'; } ?>">
+				                <a href="/events" class="">events</a>
+				            </li>
+				            <li class="<?php if( is_page('events') ) { echo 'current-cat'; } ?>">
+				                <a href="/events" class="">events</a>
+				            </li>
+				            <li class="<?php if( is_page('shop') ) { echo 'current-cat'; } ?>">
+				                <a href="/shop" class="">shop</a>
+				            </li>
+							<?php 
+								// wp_list_categories( array(
+								// 	'orderby' => 'name',
+								// 	'title_li' => '',
+								// 	'hide_title_if_empty' => true
+								// ) );
+							?>
+						</ul>
 
-			<div id="sidebar" class="sidebar">
-				<header id="masthead" class="site-header" role="banner">
+				    </nav>
 
-				    <!--
-					<div class="site-branding">
-						<?php
-							// twentyfifteen_the_custom_logo();
+				</div>
 
-							if ( is_front_page() && is_home() ) : ?>
-								<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-							<?php else : ?>
-								<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-							<?php endif;
-
-							$description = get_bloginfo( 'description', 'display' );
-							if ( $description || is_customize_preview() ) : ?>
-								<p class="site-description"><?php echo $description; ?></p>
-							<?php endif;
-						?>
-						<button class="secondary-toggle"><?php _e( 'Menu and widgets', 'retrospec' ); ?></button>
-					</div>--><!-- .site-branding -->
-
-				</header><!-- .site-header -->
-
-				<?php get_sidebar(); ?>
-			</div><!-- .sidebar -->
-
-		</div>
-
-		<div id="content" class="site-content">
-
-			<div class="container_js_trigger_menu">
-				<a href="/" title="retrospec" class="logo_site">
-					<!-- <img src="<?= get_stylesheet_directory_uri(); ?>/dist/images/logo.png" alt="retrospec logo" /> -->
-				</a>
-			</div>
-
-		</div>
+				<!-- <div id="sidebar" class="sidebar"> -->
+					<?php // get_sidebar(); ?>
+				<!-- </div> -->
