bfdffb0cdc8c39badb73f73e5e12f95e58dbd657
Dean Gilewicz <gilewiczd@gmail.com>
2018-04-08 20:55:39 -0600

    tweaked responsive styles for main nav

--- wp-content/themes/retrospec/header.php
+++ wp-content/themes/retrospec/header.php
@@ -33,68 +33,68 @@
 
 		<div class="site__wrapper"><!-- .site__wrapper -->
 
-			<div id="page" class="hfeed site"><!-- page wrapper -->
-		
-				<!-- <a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'retrospec' ); ?></a> -->
+			<!-- <a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'retrospec' ); ?></a> -->
 				
-				<div class="global__header">
+			<div class="global__header">
 
-					<div class="container__top__banner">
+				<div class="container__top__banner">
 
-						<div class="social">
-							<img src="http://via.placeholder.com/40x40?text=F" alt="facebook logo" class="social__icon" />
-							<img src="http://via.placeholder.com/40x40?text=I" alt="instagram logo" class="social__icon" />
-							<img src="http://via.placeholder.com/40x40?text=T" alt="twitter logo" class="social__icon" />
-						</div>
-
-						<form role="search" method="get" class="search__form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-							<input type="search" class="form__input" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', 'retrospec' ); ?>" value="<?php echo get_search_query(); ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label', 'retrospec' ); ?>" />
-							<input type="submit" class="form__submit" value="S" />
-						</form>
-
+					<div class="social">
+						<img src="http://via.placeholder.com/40x40?text=F" alt="facebook logo" class="social__icon" />
+						<img src="http://via.placeholder.com/40x40?text=I" alt="instagram logo" class="social__icon" />
+						<img src="http://via.placeholder.com/40x40?text=T" alt="twitter logo" class="social__icon" />
 					</div>
 
-					<div class="logo__main">
-						<a href="/">
-							<img src="http://via.placeholder.com/350x150?text=logo" alt="retrospec logo" />
-						</a>
-					</div>
-
-					<nav class="nav__main">
-						<ul class="nav__mobile">
-							<li class="nav__label">Menu</li>
-							<li class="nav__trigger">Go</li>
-						</ul>
-						<ul class="nav__items">
-							<li class="<?php if( is_home() ) { echo 'current-cat'; } ?>">
-				                <a href="/">home</a>
-				            </li>
-							<li class="<?php if( is_page('about-retrospec') ) { echo 'current-cat'; } ?>">
-				                <a href="/about-retrospec" class="">about</a>
-				            </li>
-				            <li class="<?php if( is_page('featured-fridays') ) { echo 'current-cat'; } ?>">
-				                <a href="/featured-fridays" class="">featured fridays</a>
-				            </li>
-				            <li class="<?php if( is_page('events') ) { echo 'current-cat'; } ?>">
-				                <a href="/events" class="">events</a>
-				            </li>
-				            <li class="<?php if( is_page('shop') ) { echo 'current-cat'; } ?>">
-				                <a href="/shop" class="">shop</a>
-				            </li>
-				            
-							<?php 
-								// wp_list_categories( array(
-								// 	'orderby' => 'name',
-								// 	'title_li' => '',
-								// 	'hide_title_if_empty' => true
-								// ) );
-							?>
-						</ul>
-
-				    </nav>
+					<form role="search" method="get" class="search__form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+						<input type="search" class="form__input" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', 'retrospec' ); ?>" value="<?php echo get_search_query(); ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label', 'retrospec' ); ?>" />
+						<input type="submit" class="form__submit" value="S" />
+					</form>
 
 				</div>
 
-				<!-- <div id="sidebar" class="sidebar"> -->
-					<?php // get_sidebar(); ?>
-				<!-- </div> -->
+				<div class="logo__main">
+					<a href="/">
+						<img src="http://via.placeholder.com/350x150?text=logo" alt="retrospec logo" />
+					</a>
+				</div>
+
+				<nav class="nav__main">
+					<ul class="nav__mobile">
+						<li class="nav__label">Menu</li>
+						<li class="nav__trigger">Go</li>
+					</ul>
+					<ul class="nav__items">
+						<li class="<?php if( is_home() ) { echo 'current-cat'; } ?>">
+			                <a href="/">home</a>
+			            </li>
+						<li class="<?php if( is_page('about-retrospec') ) { echo 'current-cat'; } ?>">
+			                <a href="/about-retrospec" class="">about</a>
+			            </li>
+			            <li class="<?php if( is_page('featured-fridays') ) { echo 'current-cat'; } ?>">
+			                <a href="/featured-fridays" class="">featured fridays</a>
+			            </li>
+			            <li class="<?php if( is_page('events') ) { echo 'current-cat'; } ?>">
+			                <a href="/events" class="">events</a>
+			            </li>
+			            <li class="<?php if( is_page('shop') ) { echo 'current-cat'; } ?>">
+			                <a href="/shop" class="">shop</a>
+			            </li>
+			            
+						<?php 
+							// wp_list_categories( array(
+							// 	'orderby' => 'name',
+							// 	'title_li' => '',
+							// 	'hide_title_if_empty' => true
+							// ) );
+						?>
+					</ul>
+
+			    </nav>
+
+			</div>
+
+			<!-- <div id="sidebar" class="sidebar"> -->
+				<?php // get_sidebar(); ?>
+			<!-- </div> -->
+
+			<div id="page" class="hfeed site page__wrapper"><!-- page wrapper -->
